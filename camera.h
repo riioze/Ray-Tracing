6028e33faa3f62ec2d621651c04f9f6ecc9fd7f3
lopotichaaaa <ninokeravel@gmail.com>
2024-10-12 01:26:18 +0200

    Add different materials

--- camera.h
+++ camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "hittable.h"
+#include "material.h"
 
 class camera {
     public:
@@ -89,8 +90,12 @@ class camera {
         
             hit_record rec;
             if (world.hit(r,interval(0.001,infinity),rec)){
-                vec3 direction = rec.normal+random_on_hemisphere(rec.normal);
-                return 0.5*ray_color(ray(rec.p,direction),depth-1,world);       
+                ray scattered;
+                color attenuation;
+                if (rec.mat->scatter(r, rec, attenuation, scattered)){
+                    return attenuation * ray_color(scattered,depth-1,world);
+                }
+                return color(0,0,0);
             }
 
             
--- sphere.h
+++ sphere.h
@@ -6,10 +6,8 @@
 
 class sphere : public hittable{
     public:
-        sphere(const point3& center, double radius) : center(center), radius(radius), col(){}
-        sphere(const point3& center, double radius, color sphere_color) : center(center), radius(radius), col(sphere_color){}
-
-        const color sphere_color() const {return col;} 
+        sphere(const point3& center, double radius, shared_ptr<material> mat) : center(center), radius(std::fmax(0,radius)), mat(mat){}
+        
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override{
             // resolving at2 + bt + c = 0 if there is solution(s) then the ray hit the sphere at t1 and t2
@@ -38,13 +36,15 @@ class sphere : public hittable{
             vec3 outward_normal = (rec.p-center)/radius;
             rec.set_face_normal(r,outward_normal);
 
+            rec.mat = mat;
+
             return true;
 
         }
     private:
         point3 center;
         double radius;
-        color col;
+        shared_ptr<material> mat;
             
 };
 
