9edb6ab515275f57fd3de164c557e7d0120cec40
lopotichaaaa <ninokeravel@gmail.com>
2024-11-08 21:51:22 +0100

    add dielectric material

--- main.cpp
+++ main.cpp
@@ -19,13 +19,15 @@ int main(){
 
     auto material_ground = make_shared<lambertian>(color(0.8,0.8,0.8));
     auto material_center = make_shared<lambertian>(color(0.1,0.2,0.5));
-    auto material_left   = make_shared<metal>(color(0.8,0.8,0.8),0.3);
+    auto material_left = make_shared<dielectric>(1.50); 
+    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
     auto material_right  = make_shared<metal>(color(0.8,0.6,0.2),1.0);
 
 
     world.add(make_shared<sphere>(point3(0,-100.5,-1),100.0,material_ground));
     world.add(make_shared<sphere>(point3(0,0,-1.2),0.5,material_center));
     world.add(make_shared<sphere>(point3(-1.0,0,-1.0),0.5,material_left));
+    world.add(make_shared<sphere>(point3(-1.0,0,-1.0),0.4,material_bubble));
     world.add(make_shared<sphere>(point3(1.0,0,-1.0),0.5,material_right));
     
 
@@ -33,7 +35,7 @@ int main(){
     camera cam;
     cam.aspect_ratio = 16.0/9.0;
     cam.image_width = 400;
-    cam.sample_per_pixel = 100;
+    cam.sample_per_pixel = 200;
     cam.max_depth = 50;
 
     cam.render(world);
